7880ef5e33fc31cfc73d08a17b766201ba8f87d7
katfogy <katfogy@gmail.com>
2023-07-20 16:11:10 +0100

    Rubocop error corrected

--- my_list.rb
+++ my_list.rb
@@ -1,11 +1,11 @@
 require_relative 'my_enumerable'
 class MyList
-include MyEnumerable
-def initialize(*args)
-@list=args
-end
+  include MyEnumerable
+  def initialize(*args)
+    @list = args
+  end
 
-def each(&block)
+  def each(&block)
     @list.each(&block) if block_given?
-end
+  end
 end
